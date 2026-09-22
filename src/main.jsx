37dbf1862c3cbcb5d88a30e1a38b9eb873e0f4e8
RASATO12 <rahmatsatrio26@gmail.com>
2026-09-23 04:12:58 +0700

    Fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// ponytail: one-time stale SW/cache purge; remove after all clients are on the micro-chunked build.
+async function purgeStaleServiceWorker() {
+  try {
+    if ('serviceWorker' in navigator) {
+      const regs = await navigator.serviceWorker.getRegistrations()
+      await Promise.all(regs.map((r) => r.unregister()))
+    }
+    if ('caches' in window) {
+      const keys = await caches.keys()
+      await Promise.all(keys.map((k) => caches.delete(k)))
+    }
+  } catch {}
+}
+
+purgeStaleServiceWorker().finally(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+})
